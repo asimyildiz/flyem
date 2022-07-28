4a5fbed52d8718969803c89d8a3831c10b49e511
ASIM YILDIZ <info@asimyildiz.com>
2022-07-29 02:40:51 +0300

    [release/0.0.2-SNAPSHOT] Start adding javadoc comments to code, add API documentation and Swagger for the current Spring Boot Application [feature/4] add javadoc comments to the code [feature/5] add springdoc-openapi to generate Open API documentation and Swagger to the project [feature/6] update management port to 9090

--- src/main/java/com/flyem/service/ServiceApplication.java
+++ src/main/java/com/flyem/service/ServiceApplication.java
@@ -3,9 +3,20 @@ package com.flyem.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * FlyEm Service Application
+ * 
+ * @author ASIM YILDIZ
+ * @version 0.0.1-SNAPSHOT
+ * @since 0.0.1-SNAPSHOT
+ */
 @SpringBootApplication
 public class ServiceApplication {
 
+	/**
+	 * Main method to run the service application
+	 * @param args Application arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceApplication.class, args);
 	}
--- src/main/java/com/flyem/service/greeting/GreetingController.java
+++ src/main/java/com/flyem/service/greeting/GreetingController.java
@@ -6,12 +6,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * Represents Greeting Controller for Greeting Service
+ * @author ASIM YILDIZ
+ * @version 0.0.1-SNAPSHOT
+ * @since 0.0.1-SNAPSHOT
+ */
 @Controller
 public class GreetingController {
 
+  /**
+   * Represents the static template message that Greeting Service uses
+   */
   private static final String template = "Hello from service: %s";
+  /**
+   * Represents the counter the Greeting Service uses for id property
+   */
   private final AtomicLong counter = new AtomicLong();
 
+  /**
+   * Handles /greeting request
+   * 
+   * @param name Request parameter
+   * @return Greeting response
+   * @since 0.0.1-SNAPSHOT
+   */
   @GetMapping("/greeting")
   @ResponseBody
   public Greeting sayHello(
